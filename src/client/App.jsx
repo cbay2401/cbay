2c19f1fe536953679ef7cc24e5e3fcf3e159457c
John Perkins <mrgreen@MrGreens-Laptop.local>
2024-03-14 10:21:35 -0500

    added logo

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='App'>
         <h1>Boilerplate</h1>
-        <img id='comp-img' src='./computer.png'></img>
+        <img id='logo' src='../../../media/cbay.png'></img>
         <p>Replace the starter code in this template with something cool</p>
         <Login />
     </div>
