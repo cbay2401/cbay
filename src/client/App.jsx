6ad84169b35cd790b4b4cd19acbd90211f2f2e05
Erik Rodriguez <erikrodriguez@Eriks-Air.lan>
2024-03-18 10:33:56 -0500

    register page done

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,38 +1,33 @@
-import { useState } from 'react';
-import {Routes, Route}  from "react-router-dom";
-import Login from './components/Login';
-import AllRecords from './components/AllRecords';
-import Navbar from './components/Navbar';
-import Homepage from './components/Homepage';
-import Register from './components/Register';
-import SingleRecord from './components/SingleRecord';
-
-
-
-
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import AllRecords from "./components/AllRecords";
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import Register from "./components/Register";
+import SingleRecord from "./components/SingleRecord";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [token, setToken] = useState(null);
 
   return (
     <>
-     <Navbar />
-    
+      <Navbar />
 
-
-<main>
-
-  <Routes>
-  <Route path="/" element={ <Homepage />}/>  
-  <Route path="/records" element={<AllRecords />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/register" element={<Register />} />
-  <Route path="records/:id" element={<SingleRecord />} />
-
-  </Routes>
-</main>
-   
+      <main>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/records" element={<AllRecords />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            
+            path="/register"
+            element={<Register setToken={setToken} />}
+          />
+          <Route path="records/:id" element={<SingleRecord />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/client/components/Register.jsx
+++ src/client/components/Register.jsx
@@ -1,9 +1,78 @@
+import { useState } from "react";
 
-function Register(){
+function Register({setToken}) {
+  const[firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return(
-        <h1>REGISTER</h1>
-    )
+  const [error, setError] = useState(null);
+  async function handleSubmit (event){ 
+    event.preventDefault(); 
+
+    
+    try{ 
+
+        const response = await fetch('http://localhost:3000/api/users/Register',
+        {
+            method: 'POST',
+            headers: { 
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                firstname: firstName,
+                lastname: lastName,
+                email: email,
+                password: password,
+            })
+        } );
+        
+        
+        const result = await response.json();
+
+        console.log('Registration successful', result)
+         setToken(result.token)
+         localStorage.setItem('jwtToken', result.token)
+    }catch(error){ 
+        console.error(error.message)
+    }
+}
+  return ( <>
+  <main className="register">
+      <h2> Register! </h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+              <div>
+              <label> 
+                  First Name:
+              <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+              </label>
+              </div>
+              <div>
+              <label>
+                  Last Name: 
+              <input type="text" value={lastName} onChange={(e)=> setLastName(e.target.value)}/>
+              </label>
+              </div>
+              <div>
+              <label>
+                  Email:
+              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+              </label>
+              </div>
+              <div> 
+             <label>
+                  Password:
+              <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
+              </label>
+              </div>
+
+              <button className="btn4" type="submit">Register</button>
+              
+      </form>
+  </main>
+  </>
+  )
 }
 
-export default Register
+export default Register;
