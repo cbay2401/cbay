680a05ddf7b3763b2b01d5e8926cde2e89e5b36c
John Perkins <mrgreen@MrGreens-Laptop.local>
2024-03-18 12:34:05 -0500

    added button to homepage

--- src/client/components/Homepage.jsx
+++ src/client/components/Homepage.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-//import '.style.css'; // Import your CSS file for styling
+import { useNavigate } from 'react-router-dom';
 
 const Slideshow = () => {
+
+  const navigate = useNavigate()
   const images = [
     'https://media.pitchfork.com/photos/6596f3a1cf7bb5fb106222ab/master/pass/Fiona-Apple.jpg5',
     'https://www.rollingstone.com/wp-content/uploads/2018/06/rs-146432-6b725a8c7cad1a0414c5a33f06299e9d2730ae2e.jpg',
@@ -20,6 +22,7 @@ const Slideshow = () => {
   }, [images.length]);
 
   return (
+    <>
     <div className="slideshow-container">
       {images.map((image, index) => (
         <div
@@ -31,7 +34,9 @@ const Slideshow = () => {
           <img className='slide-images' src={image} alt={`Slide ${index}`} />
         </div>
       ))}
+      <button id='homepage-button' onClick={() => navigate("/records")}>See All Records</button>
     </div>
+    </>
   );
 };
 
