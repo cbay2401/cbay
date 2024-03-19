899ab0f01f33a3ea8f9458e281e64766261c0195
Mitch Wells <mitchwells@Mitchs-iMac.MG8702>
2024-03-19 12:40:37 -0500

    working on cart backend

--- src/server/api/orders.js
+++ src/server/api/orders.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const ordersRouter = express.Router();
-const { getAllOrders, createOrder } = require('../db/orders');
+const { getAllOrders, createOrder, createCart } = require("../db/orders");
 
-ordersRouter.get('/admin/orders', async (req, res, next) => {
+ordersRouter.get("/admin/orders", async (req, res, next) => {
   try {
     const orders = await getAllOrders();
     res.json(orders);
@@ -11,11 +11,22 @@ ordersRouter.get('/admin/orders', async (req, res, next) => {
   }
 });
 
-ordersRouter.post('/orders', async (req, res, next) => {
+ordersRouter.post("/orders", async (req, res, next) => {
   const { userId, orderDate, shippingAddress, status } = req.body;
   try {
     const order = await createOrder(userId, orderDate, shippingAddress, status);
     res.status(201).json(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+ordersRouter.patch("/orders/orderId", async (req, res, next) => {
+  const order_Id = req.params.order_Id;
+  const update = req.body;
+  try {
+    const updateOrder = await updateOrder(order_Id, update);
+    res.json(updateOrder);
   } catch (error) {
     next(error);
   }
--- src/server/db/orders.js
+++ src/server/db/orders.js
@@ -2,7 +2,7 @@ const db = require("./client");
 
 async function getAllOrders() {
   try {
-    const { rows } = await db.query('SELECT * FROM orders');
+    const { rows } = await db.query("SELECT * FROM orders");
     return rows;
   } catch (error) {
     throw error;
@@ -23,8 +23,37 @@ async function createOrder(userId, orderDate, shippingAddress, status) {
   }
 }
 
+async function createCart(order_Id, records_Id, quantity) {
+  try {
+    const { rows } = await db.query(
+      `INSERT INTO orders_records ( order_id, records_id, quantity)
+      VALUES ($1, $2, $3)
+      RETURNING *`,
+      [(order_Id, records_Id, quantity)]
+    );
+    return rows[0];
+  } catch (err) {
+    throw error;
+  }
+}
+
+async function updateOrder(order_Id, updates){
+
+  const {status, shippingAddress} = updates 
+  try{
+    const {rows} =  await db.query
+    (`UPDATE orders SET status=$1 ,shippingaddress=$2 WHERE id= $3 RETURNING *`,
+    [status, shippingAddress, order_Id])
+    return rows[0];
+  }catch (err){
+    console.log("Error in updating the Order")
+    throw err
+  }
+}
 
 module.exports = {
   getAllOrders,
   createOrder,
+  createCart,
+  updateOrder
 };
