643d473e36de0ca71e30dd03084c934084acb9fd
Carly Zimmerman <carlyzimmerman@Carlys-MacBook-Air.local>
2024-03-20 19:10:44 -0600

    commenting out account 2 and require token 2

--- src/server/api/requireToken 2.js
+++ src/server/api/requireToken 2.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken')
-const { getUserById } = require('../db/users')
+// const jwt = require('jsonwebtoken')
+// const { getUserById } = require('../db/users')
 
-const requireToken = async (req, res, next) => {
-  try {
-    // Verify the user is logged in, otherwise throw an error
-    const payload = jwt.verify(req.headers.authorization, process.env.JWT)
+// const requireToken = async (req, res, next) => {
+//   try {
+//     // Verify the user is logged in, otherwise throw an error
+//     const payload = jwt.verify(req.headers.authorization, process.env.JWT)
 
-    // Find the user data in the database
-    const user = await getUserById(payload.id)
+//     // Find the user data in the database
+//     const user = await getUserById(payload.id)
 
-    // Add the user data to the `req` object for easy reference in our routes
-    req.user = user
+//     // Add the user data to the `req` object for easy reference in our routes
+//     req.user = user
 
-    // next() means we are not done processing the request - on to our route code!
-    next()
-  } catch(error) {
-    console.error(error.stack);
-    res.status(401).send('not logged in');
-  }
-};
+//     // next() means we are not done processing the request - on to our route code!
+//     next()
+//   } catch(error) {
+//     console.error(error.stack);
+//     res.status(401).send('not logged in');
+//   }
+// };
 
-module.exports = requireToken;
+// module.exports = requireToken;
