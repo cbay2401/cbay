27892de58d1519985f1f9926ca2cb286a3ee65d5
jonathan bushapa <jonathanbushapa@Mac-mini-7.home>
2024-03-22 14:45:29 -0500

    updated allrecors css/js

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -39,7 +39,7 @@ function App() {
   <Route path="/admin" element={<AdminDashboard token={token}/>} />
   </Routes>
 </main>
-   <Footer />
+   {/* <Footer /> */}
     </>
   );
 }
--- src/server/api/index.js
+++ src/server/api/index.js
@@ -72,6 +72,7 @@ const { getUserByEmail } = require('../db');
 apiRouter.use('/users', usersRouter);
 
 apiRouter.use((err, req, res, next) => {
+    console.log(err)
     res.status(500).send(err)
   })
 
