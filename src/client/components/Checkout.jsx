8a1b3b3bf62898c338cc5b4bbaad01dcb6bdcd68
Carly Zimmerman <carlyzimmerman@Carlys-MacBook-Air.local>
2024-03-24 11:08:27 -0600

    css for checkout

--- src/client/components/Checkout.jsx
+++ src/client/components/Checkout.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 
 function CheckoutForm() {
   const { cartId } = useParams();
   const [formData, setFormData] = useState({
-    name: '',
-    address: '',
-    creditCardNumber: '',
-    expiryDate: '',
-    cvv: ''
+    name: "",
+    address: "",
+    creditCardNumber: "",
+    expiryDate: "",
+    cvv: "",
   });
- const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -26,73 +26,77 @@ function CheckoutForm() {
     try {
       // Delete all items in the cart
       await axios.delete(`/api/orders/cart/ck/${cartId}`);
-      console.log('All items in the cart have been deleted successfully!');
+      console.log("All items in the cart have been deleted successfully!");
     } catch (error) {
-      console.error('Error deleting items from the cart:', error);
+      console.error("Error deleting items from the cart:", error);
     }
     // You can handle form submission here, e.g., send other data to backend or perform validation
-    console.log('Form submitted:', formData);
-    navigate("/success")
+    console.log("Form submitted:", formData);
+    navigate("/success");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="address">Shipping Address:</label>
-        <textarea
-          id="address"
-          name="address"
-          value={formData.address}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="creditCardNumber">Credit Card Number:</label>
-        <input
-          type="text"
-          id="creditCardNumber"
-          name="creditCardNumber"
-          value={formData.creditCardNumber}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="expiryDate">Expiry Date:</label>
-        <input
-          type="text"
-          id="expiryDate"
-          name="expiryDate"
-          value={formData.expiryDate}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="cvv">CVV:</label>
-        <input
-          type="text"
-          id="cvv"
-          name="cvv"
-          value={formData.cvv}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="checkout-form">
+      <h2 className="login-text">Checkout</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="address">Shipping Address:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="creditCardNumber">Credit Card Number:</label>
+          <input
+            type="text"
+            id="creditCardNumber"
+            name="creditCardNumber"
+            value={formData.creditCardNumber}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="expiryDate">Expiry Date:</label>
+          <input
+            type="text"
+            id="expiryDate"
+            name="expiryDate"
+            value={formData.expiryDate}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="cvv">CVV:</label>
+          <input
+            type="text"
+            id="cvv"
+            name="cvv"
+            value={formData.cvv}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
