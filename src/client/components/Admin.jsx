7d45835f56ce9f2c5796ca9568eb65828e4a6db5
jonathan bushapa <jonathanbushapa@Mac-mini-7.home>
2024-03-26 09:54:32 -0500

    changed and rebuild admin/records

--- src/client/components/Admin.jsx
+++ src/client/components/Admin.jsx
@@ -87,28 +87,51 @@ const AdminDashboard = () => {
   );
   return (
     <div>
-      <h2>Admin Dashboard</h2>
-      <button className="admin-btn" onClick={handleViewUsers}>
-        View All Users
-      </button>
-      <button className="admin-btn" onClick={handleViewRecords}>
-        View All Records
-      </button>
-      {showUsers === true && users.length > 0 && (
-        <div>
-          <h3>All Users</h3>
-          <ul>
-            {users.map((user) => (
-              <li className="users-list" key={user.id}>
-                {user.name}
-              </li>
-            ))}
-          </ul>
+        <div className="admin-dashboard">
+          
+          <h2>Admin Dashboard</h2>
+          <div className="admin-bttn-container">
+            <button className="admin-btn" onClick={handleViewUsers}>
+              <span>View All Users</span>
+            </button>
+            <button className="admin-btn" onClick={handleViewRecords}>
+              <span>View All Records</span>
+            </button>
+          </div>
+         
+          {showUsers === true && users.length > 0 && (
+           <div className="admin-container">
+
+            
+            <div className="admin-content">
+              <h3>All Users</h3>
+             
+                <table className="table-container">
+                    <tr>
+                      <th></th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Role</th>
+                    </tr>  
+                    {users.map((user) => (
+
+                    <tr key={user.id}>
+                      <td><img className="admin-svg" src="../../../media/person.svg" alt="admin" /></td>
+                      <td> <div className="admin-name">{user.name}</div></td>
+                      <td><div className="admin-name">{user.email}</div></td>
+                      <td><div className="admin-name">{user.role}</div></td>
+                    </tr>
+                     ))} 
+                </table>
+            
+            </div>
         </div>
       )}
+      </div>
+      
       {showUsers === false && (
         <div>
-          <h3>All Records</h3>
+          <h3 id="all-records-header">All Records</h3>
           <section className="searchbar-container">
             <input
               id="searchbar"
@@ -123,6 +146,7 @@ const AdminDashboard = () => {
 
             <form className="add-record-form" onSubmit={handleAddRecord}>
               <h2>Add New Record</h2>
+              <div className="add-record-form-content">
               <label>
                 Artist:
                 <input
@@ -183,27 +207,29 @@ const AdminDashboard = () => {
                   }
                 />
               </label>
+              
               <button className="admin-sub" type="submit">
                 Add Record
               </button>
+              </div>
             </form>
             {/* Display existing records */}
             {filteredRecords.map((record) => (
-              <div key={record.id} className="records">
-                <img
-                  className="albumcover"
-                  src={record.imageurl}
-                  alt="Album Cover"
-                />
-                <h1>{record.artist}</h1>
-                <h2>{record.albumname}</h2>
-                <p>${record.price}</p>
-                <button
-                  className="admin-del"
-                  onClick={() => handleDeleteRecord(record.id)}
-                >
-                  Delete Record
-                </button>
+              <div key={record.id} className="admin-records">
+                <img className="albumcover" src={record.imageurl} alt="Album Cover" />
+
+                <div className="admin-records-text">
+                    <h4>{record.artist}</h4>
+                    <h5>{record.albumname}</h5>
+                    <p>${record.price}</p>
+                </div>
+
+                <div className="admin-del-container">
+                    <button className="admin-del" onClick={() => handleDeleteRecord(record.id)}>
+                      Delete Record
+                    </button>
+                </div>
+              
               </div>
             ))}
           </section>
