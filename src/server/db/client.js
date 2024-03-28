74a05d84408874df7ceeaced930396579009b927
Your Name <you@example.com>
2024-03-28 10:29:45 -0500

    update

--- src/server/db/client.js
+++ src/server/db/client.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const { Client } = require('pg');
 const connectionString = process.env.DATABASE_URL || 'http://localhost:5432/cbay';
 
 const db = new Client({
     connectionString,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+    //ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = db;
